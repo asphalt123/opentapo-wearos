49d4ec00007fcb3c3d167490452dbfbe6a9fbe9d
asphalt123 <asphalt123@users.noreply.github.com>
2026-09-09 21:10:48 +0000

    feat(mobile): minuteries countdown + horaires recurrents avec notifs et replanification boot

--- OpenTapoWearOs/mobile/src/main/java/dev/veeso/opentapo/mobile/MainActivity.kt
+++ OpenTapoWearOs/mobile/src/main/java/dev/veeso/opentapo/mobile/MainActivity.kt
@@ -94,6 +94,10 @@ class MainActivity : AppCompatActivity() {
                     startActivity(Intent(this, dev.veeso.opentapo.mobile.tile.TileSettingsActivity::class.java))
                     true
                 }
+                R.id.action_timers -> {
+                    startActivity(Intent(this, dev.veeso.opentapo.mobile.timer.TimerActivity::class.java))
+                    true
+                }
                 else -> false
             }
         }
